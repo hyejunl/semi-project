904e3dc51fa7c52e90ec75e0e06c9b9722f562d2
semi-project <jn318@naver.com>
2019-12-16 09:07:35 +0900

    안심귀가

---
+++ semi_project.R
@@ -0,0 +1,130 @@
+install.packages("readxl")
+install.packages("xlsx")
+install.packages("dplyr")
+install.packages("stringr")
+# map 만들때 필요한 packages
+install.packages("ggmap")
+install.packages("raster")
+install.packages("rgeos")
+install.packages("maptools")
+install.packages("rgdal")
+install.packages("viridis")
+install.packages("ggplot2")
+install.packages("devtools")
+install.packages("plotly")
+install.packages("dygraphs")
+
+library(readxl)
+library(xlsx)
+library(dplyr)
+library(stringr)
+library(ggmap)
+library(raster)
+library(rgeos)
+library(maptools)
+library(rgdal)
+library(viridis)
+library(ggplot2)
+library(devtools)
+library(plotly)
+library(dygraphs)
+############################### 파일 불러오기
+
+# 구별 인구 수 -> pop
+population_file = "C:/Users/LHJ/Desktop/안심귀가 project/구별 인구 수.xls"
+raw_population <- read_excel(path = population_file)
+pop <- as.data.frame(raw_population)
+pop <- rename(pop,gu=...1)
+View(pop);str(pop)
+
+# 구별 연도별 범죄 발생 건 수 ->crime
+crime_file = "C:/Users/LHJ/Desktop/안심귀가 project/구별 연도별 범죄 발생 건수.xls"
+raw_crime <- read_excel(path = crime_file)
+crime <- as.data.frame(raw_crime)
+crime <- rename(crime,
+                gu=...1,
+                c_14=`2014`,
+                c_15=`2015`,
+                c_16=`2016`,
+                c_17=`2017`,
+                c_18=`2018`)
+View(crime);str(crime)
+
+# 구별 경찰서 개소 수 -> police
+police_file = "C:/Users/LHJ/Desktop/안심귀가 project/서울시 구별 경찰관서 개소 수.xls"
+raw_police <- read_excel(path = police_file)
+police <- as.data.frame(raw_police)
+police <- rename(police,
+                 gu=...1,
+                 p_station=경찰서,
+                 p_substation=지구대파출소치안센터)
+View(police);str(police)
+
+# 구별 연도별 cctv 개수 -> cctv
+cctv_file = "C:/Users/LHJ/Desktop/안심귀가 project/서울시 자치구 년도별 CCTV 설치 현황(2011년 이전_2018년).xlsx"
+raw_cctv <- read_excel(path = cctv_file)
+cctv <- as.data.frame(raw_cctv)
+cctv <- rename(cctv,
+                gu=...1,
+                cam_14=`2014`,
+                cam_15=`2015`,
+                cam_16=`2016`,
+                cam_17=`2017`,
+                cam_18=`2018`)%>%
+  select("gu","cam_14","cam_15","cam_16","cam_17","cam_18")
+View(cctv);str(cctv)
+
+
+
+#######################1. 10만명 당 범죄 건수가 많은 구(pop,crime 사용)->gu_crime
+gu_crime <- left_join(pop,crime)%>%
+  select("gu","population","c_18")%>%
+  mutate(gu_crime=c_18/(population/100000))%>%
+  arrange(desc(gu_crime))
+str(gu_crime);View(gu_crime)
+
+
+#######################2. CCTV로도 범죄가 줄어들지 않는 구(crime,cctv사용)->cctv_cor
+# 구 이름 통일해주기
+for (i in 1:length(cctv$gu)){
+  cctv$gu[i]<-str_replace_all(cctv$gu[i]," ","")
+}
+# 구 이름 순서대로 맞춰주기
+cctv<-cctv%>%arrange(gu)
+crime<-crime%>%arrange(gu)
+View(cctv);View(crime)
+
+# cor를 하기 위해 data 형식 바꿔주기
+cctv_f<-as.data.frame(t(cctv),stringsAsFactors = F)
+crime_f<-as.data.frame(t(crime),stringsAsFactors = F)
+
+gu = cctv$gu
+cor_v = c()
+for (i in 1:length(gu)){
+  cor_v[i] <- cor(as.numeric(cctv_f[2:6,i]),as.numeric(crime_f[2:6,i]))
+}
+cor_v
+cctv_cor<-as.data.frame(cbind(gu,cor_v))
+cctv_cor<- cctv_cor%>%
+  arrange(cor_v)
+
+
+
+#######################3. 10만명 당 지구대, 경찰서 수가 적은 구(pop,police 사용)->gu_police
+gu_police <- left_join(pop,police)%>%
+  mutate(gu_police=(p_station+p_substation)/(population/100000))%>%
+  arrange(gu_police)
+View(gu_police)
+
+###########################################gu_crime 시각화
+???
+
+
+####################### 어떤 지역 선택??
+head(gu_crime)
+gu_police
+head(cctv_cor)
+# cctv와 범죄건수의 상관계수가 -0.5이상이며,
+# 구별 범죄건수 4위
+# ??? 10만명 당 지구대, 경찰서 수가 21위인데 어떻게 연관지을지... 
+
---
+++ 여성 안심 귀가 순찰 적합지 분석 project.md
@@ -0,0 +1,39 @@
+# 여성 안심 귀가 순찰 적합지 분석 (map 그리기)
+
+### 1. 10만명당 범죄건수가 많은 지역
+
+-[서울시 5대 범죄 발생현황 통계](https://data.seoul.go.kr/dataList/datasetView.do?infId=316&srvType=S&serviceKind=2) *2018년 데이터
+
+-[서울시 주민등록인구 (구별) 통계](https://data.seoul.go.kr/dataList/datasetView.do?serviceKind=2&infId=419&srvType=S&stcSrl=419) *2018년 데이터
+
+
+### 2. CCTV 설치로도 범죄가 줄어들지 않는 지역 **(cctv가 아닌 순찰이 꼭 필요한 곳)**
+
+-[서울시 자치구 년도별 CCTV 설치 현황](https://data.seoul.go.kr/dataList/datasetView.do?infId=OA-2734&srvType=F&serviceKind=1&currentPageNo=1) *2014~2018년 데이터
+
+-[서울시 5대 범죄 발생현황 통계](https://data.seoul.go.kr/dataList/datasetView.do?infId=316&srvType=S&serviceKind=2) *2014~2018년 데이터
+
+### 3. 10만명당 지구대, 경찰서 수가 적은 지역
+
+-[서울시 시내주요기관 (경찰·소방관서) 통계](https://data.seoul.go.kr/dataList/datasetView.do?infId=224&srvType=S&serviceKind=2) *2018년 데이터 
+
+-[서울시 주민등록인구 (구별) 통계](https://data.seoul.go.kr/dataList/datasetView.do?serviceKind=2&infId=419&srvType=S&stcSrl=419) *2018년 데이터
+
+---
+
+위의 3가지에 모두 해당하는 지역 한곳을 여성 안심 귀가 순찰 적합지로 판단.
+
+**cctv와 범죄건수의 상관계수가 -0.5 이상이고,구별 범죄 건수 4위인 '용산구'를 안심 귀가 순찰 적합지로 판단.** 
+
+
+
+
+
+그 지역의 귀가시간대 유동인구를 파악 유동인구가 적은 곳을 순찰이 필요한 지역으로 정하고, 순찰 확대
+
+---
+
+아쉬운 점 : 여성범죄 다발지역, 위치 데이터가 있다면 조금 더 적합한 지역을 선정할 수 있었을 것... 
+
+ 
+
